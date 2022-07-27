c519da22c8e98d098dd94048f6696c4cf92a8e40
kramo <93832451+kra-mo@users.noreply.github.com>
2022-07-27 11:49:55 +0200

    Files

---
+++ extension.js
@@ -0,0 +1,72 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const GETTEXT_DOMAIN = 'my-indicator-extension';
+
+const { Gio, GObject, St } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const _ = ExtensionUtils.gettext;
+
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, _('Touchpad Indicator'));
+
+        this.schema = Gio.Settings.new('org.gnome.desktop.peripherals.touchpad');
+
+        this.add_child(new St.Icon({
+            icon_name: 'input-touchpad-symbolic',
+            style_class: 'system-status-icon',
+        }));
+
+        let item = new PopupMenu.PopupMenuItem(_('Toogle Touchpad'));
+        item.connect('activate', () => {
+            this.schema.set_boolean('disable-while-typing', !this.schema.get_boolean('disable-while-typing'));
+        });
+        this.menu.addMenuItem(item);
+    }
+});
+
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+
+        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+    }
+
+    enable() {
+        this._indicator = new Indicator();
+        Main.panel.addToStatusArea(this._uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
+
+function init(meta) {
+    return new Extension(meta.uuid);
+}
